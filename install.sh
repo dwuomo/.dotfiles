6bc175eeef742eb6f035324ed1f8b3a6aa1dc631
dwuomo <dwuomo@gmail.com>
2014-04-08 22:01:15 +0200

    bash install file

---
+++ install.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+echo "initializing submodules"
+git submodule init
+
+echo "updating submodules"
+git submodule update
+
+echo "creating symlinks"
+ln -s ~/.dotfiles/.vim ~/.vim
+ln -s ~/.dotfiles/.vim/vimrc ~/.vimrc
+ln -s ~/.dotfiles/.bashrc ~/.bashrc
+ln -s ~/.dotfiles/.bash_aliases ~/.bash_aliases
+echo "-------- done! ---------"
