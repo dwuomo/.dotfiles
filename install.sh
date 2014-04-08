f3193e6abfe697c74848a741e209bdcf9e64e0e1
dwuomo <dwuomo@gmail.com>
2014-04-08 22:49:46 +0200

    install.dh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ git submodule update
 
 echo "creating symlinks"
 ln -s ~/.dotfiles/.vim ~/.vim
-ln -s ~/.dotfiles/.vim/.vimrc ~/.vimrc
+ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/.bashrc ~/.bashrc
 ln -s ~/.dotfiles/.bash_aliases ~/.bash_aliases
 echo "-------- done! ---------"
